3c35ad759c73d54e06c540b2a2dcda028a342028
Dingle Varshney <dinglevarshney12@gmail.com>
2025-12-20 14:26:31 +0530

    error resolve in api

--- app/api/project/[id]/route.ts
+++ app/api/project/[id]/route.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { authOptions } from "../../auth/[...nextauth]/route";
 import connectDB from "@/database/mongodb";
 import Project from "@/models/project.model";
